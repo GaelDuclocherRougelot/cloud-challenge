41b2eb496884044c25a6fb18764cf44e2330ac39
Dylan <dylan77120@live.fr>
2024-03-19 12:28:27 +0100

    login/register : creation du form de register et login

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import {Route, Routes} from "react-router-dom";
 import Challenges from "./pages/Challenges/Challenges.jsx";
-import Home from "./pages/Home/Home.jsx";
 import Contribution from "./pages/Contribution/Contribution.jsx";
+import Login from "./pages/Login/Login.jsx";
+import Home from "./pages/Home/Home.jsx";
+import Register from "./pages/Register/Register.jsx";
 
 function App() {
 
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/challenges" element={<Challenges />} />
         <Route path="/contribution" element={<Contribution />} />
       </Routes>
