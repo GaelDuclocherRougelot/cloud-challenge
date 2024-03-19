d6b90f14c231df5361a8ea465d33eed0269a0a16
supersixaxis <appert.nathan@outlook.fr>
2024-03-19 14:14:58 +0100

    homepage header

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,6 +6,9 @@ const Home = () => {
       <div className='flex bg-red-500'>
         <h1>Accueil</h1>
       </div>
+      <div className='flex'>
+      <td>
+        Top 3 de la semaine
       <tr>
           John doe
       </tr>
@@ -15,8 +18,15 @@ const Home = () => {
       <tr>
           John doe
       </tr>
+      </td>
+      <p>Challenge réalisés cette semaine</p>
+      <p>12</p>
+      </div>
+      <input type="text" placeholder='Chercher un challenge...' />
     </header>
       
+
+      // faire une boucle .map pour afficher les challenges
   );
 };
 
