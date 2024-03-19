9a32be40a9525fd1b76b8c634c078b7edf4f8221
Gaël Duclocher-Rougelot <gaeldr@ordinateur-portable-de-gael.home>
2024-03-19 14:22:08 +0100

    fix: for merge

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import Navbar from '../../components/Navbar/Navbar'
+import React from 'react';
 
 const Home = () => {
   return (
-    <div className="flex">
-      <Navbar />
-    </div>
-  )
-}
+      <div className='flex bg-red-500'>
+        <h1>Home</h1>
+      </div>
+  );
+};
 
-export default Home
+export default Home;
