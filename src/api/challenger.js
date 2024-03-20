08592e40a09922ae5c547d6eeab427576d632499
Dylan <dylan77120@live.fr>
2024-03-20 13:54:18 +0100

    feature deconnexion mis en place

--- src/api/challenger.js
+++ src/api/challenger.js
@@ -1,7 +1,5 @@
-import { collection, addDoc, getDocs, query, where } from "firebase/firestore";
+import { collection, addDoc} from "firebase/firestore";
 import {db} from '../utils/firebase_init.js';
-import {useNavigate} from "react-router-dom";
-
 
 export const createChallenger = async (id , lastName, firstName) => {
   try {
@@ -16,24 +14,6 @@ export const createChallenger = async (id , lastName, firstName) => {
     console.log("Document written with ID: ", docRef.id);
   } catch (e) {
     console.error("Error adding document: ", e);
-  }
-}
-
-export const login = async (email, password) => {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const navigate = useNavigate();
-  try {
-    const usersQuery = query(collection(db, "Challenger"), where("email", "==", email), where("password", "==", password));
-    const userSnapshot = await getDocs(usersQuery);
-    const userExists = !userSnapshot.empty;
-
-    if (userExists) {
-      navigate('/home');
-    } else {
-      console.error("Email ou mot de passe incorrect");
-    }
-  } catch (e) {
-    console.error("Erreur lors de la connexion : ", e);
   }
 }
 
