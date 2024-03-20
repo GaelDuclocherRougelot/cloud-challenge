cf71e2ab3512afe9f6211c49d5fd62ea988ab692
Gaël Duclocher-Rougelot <gaelduclocher.rougelot@gmail.com>
2024-03-20 13:37:53 +0100

    add .env in gitignore

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,30 +1,26 @@
 const Home = () => {
   return (
     <header>
-      <div className='flex bg-red-500'>
+      <div className="flex bg-red-500">
         <h1>Accueil</h1>
       </div>
-      <div className='flex justify-between'>
-      <td>
-        Top 3 de la semaine
-      <tr>
-          John doe
-      </tr>
-      <tr>
-          John doe
-      </tr>
-      <tr>
-          John doe
-      </tr>
-      </td>
-      <p>Challenge réalisés cette semaine <br/><span>12</span></p>
+      <div className="flex justify-between">
+        <td>
+          Top 3 de la semaine
+          <tr>John doe</tr>
+          <tr>John doe</tr>
+          <tr>John doe</tr>
+        </td>
+        <p>
+          Challenge réalisés cette semaine <br />
+          <span>12</span>
+        </p>
       </div>
-      <input type="text" placeholder='Chercher un challenge...' />
+      <input type="text" placeholder="Chercher un challenge..." />
     </header>
-      
 
-      // faire une boucle .map pour afficher les challenges
-  );
-};
+    // faire une boucle .map pour afficher les challenges
+  )
+}
 
 export default Home
