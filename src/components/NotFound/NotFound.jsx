eff8a7adf013c0c1b41cb89487037323d8e99232
supersixaxis <appert.nathan@outlook.fr>
2024-03-21 16:58:13 +0100

    esthetic page 404

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,9 +1,9 @@
-
-
 export default function NotFound() {
   return (
     <div>
-      Page non trouvée erreur 404
+      <h1 className="text-6xl text-center pt-80 text-red-600 font-semibold">
+        Page non trouvée erreur 404
+      </h1>
     </div>
   )
 }
