84a44844ced52096317bd36662e921a5dbbe05af
supersixaxis <appert.nathan@outlook.fr>
2024-03-21 11:44:20 +0100

    nettoyage code

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,7 +1,6 @@
-import React, { createContext, useEffect, useReducer, useContext, useState } from "react";
+import { createContext, useEffect, useContext, useState } from "react";
 import Loader from "../components/Loader/Loader.jsx";
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth, app } from "../utils/firebase_init.js";
+import { auth } from "../utils/firebase_init.js";
 import { getChallengerById } from "../api/challenger.js";
 
 export const AuthContext = createContext();
@@ -13,24 +12,18 @@ export const AuthContextProvider = ({ children }) => {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    // Votre code de gestion de l'authentification ici, s'il y en a
-
-    // Par exemple, si vous vérifiez si l'utilisateur est connecté, vous pouvez utiliser onAuthStateChanged :
     const unsubscribe = auth.onAuthStateChanged(async (user) => {
       if (user) {
         try {
-          // Récupérez les données de l'utilisateur après sa connexion
           const challengerData = await getChallengerById(user.uid);
-          // Mettez à jour l'état currentUser avec les données de l'utilisateur
           setCurrentUser(challengerData);
         } catch (error) {
           console.error("Erreur lors de la récupération des données de l'utilisateur :", error.message);
         }
       }
-      setIsLoading(false); // Mettez isLoading à false une fois que vous avez fini de vérifier l'authentification
+      setIsLoading(false);
     });
 
-    // Nettoyez l'abonnement lorsque le composant est démonté
     return () => unsubscribe();
   }, []);
 
